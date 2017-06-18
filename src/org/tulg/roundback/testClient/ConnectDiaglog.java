1b6dada426e52b255db17c38c7a35f277cf83874
Jason Williams <jasonw@jhu.edu>
2017-06-18 19:57:13 -0400

    UI formatting and updates for the upcoming storage code implementation

--- src/org/tulg/roundback/testClient/ConnectDiaglog.java
+++ src/org/tulg/roundback/testClient/ConnectDiaglog.java
@@ -3,25 +3,71 @@ package org.tulg.roundback.testClient;
 import org.tulg.roundback.client.ClientNetwork;
 
 import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 import java.io.IOException;
+import java.util.*;
+import java.util.List;
 
 class ConnectDiaglog extends JDialog {
     private JPanel contentPane;
     private JButton buttonConnect;
     private JButton buttonClose;
     private JButton btnPrefs;
-    private JTextArea textArea1;
+
     private JTextField txtCommand;
     private JButton btnSend;
+    private JEditorPane editorPane1;
+    private JButton btnFile;
     private final TestCliConfig testCliConfig;
+    private StyledDocument styledDocument;
+    SimpleAttributeSet defaultStyle;
+    private SimpleAttributeSet boldText;
     private ClientNetwork clientNetwork;
     private boolean connected;
     private Thread recvThread;
+    private static final String title = "RoundBack Test Client";
+
+    /*
+            new ImageIcon("icon_32.png").getImage(),
+            new ImageIcon("icon_64.png").getImage());*/
 
 
     public ConnectDiaglog(TestCliConfig testCliConfig) {
+
+        super(new TestClientFrame(title, null));
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource("RoundBack.png"));
+        Image image = imageIcon.getImage();
+
+        btnSend.setMnemonic('s');
+        buttonConnect.setMnemonic('n');
+        buttonClose.setMnemonic('c');
+        btnPrefs.setMnemonic('p');
+
+
+        editorPane1.setBackground(Color.BLACK);
+        editorPane1.setForeground(Color.GREEN);
+        editorPane1.setCaretColor(Color.GREEN);
+        editorPane1.getCaret().setVisible(true);
+        editorPane1.getCaret().setBlinkRate(500);
+
+        styledDocument = (StyledDocument) editorPane1.getDocument();
+        defaultStyle = new SimpleAttributeSet();
+        StyleConstants.setFontFamily(defaultStyle, "Monospace");
+        StyleConstants.setForeground(defaultStyle, Color.GREEN);
+        StyleConstants.setBackground(defaultStyle, Color.BLACK);
+
+        boldText = new SimpleAttributeSet(defaultStyle);
+        StyleConstants.setBold(boldText, true);
+
+        ((TestClientFrame)getParent()).setIconImage(image);
+        setIconImage(image);
+
+        setTitle(title);
         connected = false;
+        btnFile.setEnabled(false);
         this.testCliConfig = testCliConfig;
 
         setContentPane(contentPane);
@@ -32,6 +78,7 @@ class ConnectDiaglog extends JDialog {
         buttonClose.addActionListener(e -> onClose());
         btnPrefs.addActionListener(e -> onPrefs());
         btnSend.addActionListener(e -> onSend());
+        btnFile.addActionListener(e -> onFile());
 
         resetThread();
 
@@ -46,8 +93,24 @@ class ConnectDiaglog extends JDialog {
 
         // call onClose() on ESCAPE
         contentPane.registerKeyboardAction(e -> onClose(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+
+
     }
 
+    private void onFile() {
+        FileTransfer fileTransfer = new FileTransfer(testCliConfig);
+        fileTransfer.setConnectDiaglog(this);
+        fileTransfer.pack();
+        fileTransfer.setVisible(true);
+
+    }
+
+    public void setVisible(boolean visible) {
+        super.setVisible(visible);
+        if(!visible) {
+            ((TestClientFrame)getParent()).dispose();
+        }
+    }
     private void resetThread(){
         // set up a new thread to do the recv
         recvThread = new Thread(() -> {
@@ -56,23 +119,33 @@ class ConnectDiaglog extends JDialog {
                 while(connected) {
                     // if we ar econnected
                     try {
-                        textArea1.append(clientNetwork.recvRaw() + "\n");
+                        String tmpString = clientNetwork.recvRaw();
+                        if(tmpString == null)
+                            throw new IOException("Disconnected.");
+                        if(!tmpString.equals("")) {
+                            styledDocument.insertString(styledDocument.getLength(), tmpString + "\n", defaultStyle);
+                            //editorPane1.setText(editorPane1.getText() + tmpString + "\n");
+                        }
                     } catch (IOException e) {
                         connected = false;
+                        btnFile.setEnabled(false);
                         buttonConnect.setText("Connect");
                         clientNetwork.disconnect();
                         clientNetwork = null;
                     } catch (InterruptedException e) {
                         connected = false;
+                        btnFile.setEnabled(false);
                         buttonConnect.setText("Connect");
                         if(clientNetwork != null) {
                             clientNetwork.disconnect();
                             clientNetwork = null;
                         }
+                    } catch (BadLocationException e) {
+                        e.printStackTrace();
                     }
                 }
             }
-            System.out.println("Recieve thread exit.");
+            System.out.println("Receive thread exit.");
             resetThread();
         });
     }
@@ -81,9 +154,12 @@ class ConnectDiaglog extends JDialog {
         // Send the entered command
         if(clientNetwork != null) {
             try {
+                styledDocument.insertString(styledDocument.getLength(),
+                         ">>> " + txtCommand.getText() + "\n", boldText  );
+                //editorPane1.setText(editorPane1.getText() + "<b>>>> " + txtCommand.getText()+ "</b>\n");
                 clientNetwork.sendRaw(txtCommand.getText());
                 txtCommand.setText("");
-            } catch (IOException | NullPointerException e) {
+            } catch (IOException | NullPointerException | BadLocationException e) {
                 e.printStackTrace();
             }
         }
@@ -105,15 +181,17 @@ class ConnectDiaglog extends JDialog {
             clientNetwork.setServer(testCliConfig.getHostname(), testCliConfig.getPort());
             if (clientNetwork.connect()) {
                 connected = true;
+                btnFile.setEnabled(true);
                 buttonConnect.setText("Disconnect");
                 contentPane.setPreferredSize(contentPane.getPreferredSize());
                 recvThread.start();
             }
         } else {
             connected = false;
+            btnFile.setEnabled(false);
             buttonConnect.setText("Connect");
             clientNetwork.disconnect();
-            clientNetwork = null;
+            //clientNetwork = null;
             recvThread.interrupt();
         }
 
@@ -123,13 +201,26 @@ class ConnectDiaglog extends JDialog {
         // add your code here if necessary
         if(connected){
             connected = false;
+            btnFile.setEnabled(false);
             buttonConnect.setText("Connect");
             clientNetwork.disconnect();
-            clientNetwork = null;
+            //clientNetwork = null;
             recvThread.interrupt();
 
         }
         dispose();
+        ((TestClientFrame)getParent()).dispose();
+        System.exit(0);
     }
 
+}
+
+class TestClientFrame extends JFrame {
+    TestClientFrame(String title, java.util.List<? extends Image> iconImages){
+        super(title);
+        setUndecorated(true);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setIconImages(iconImages);
+    }
 }
---
+++ src/org/tulg/roundback/testClient/FileTransfer.java
@@ -0,0 +1,185 @@
+package org.tulg.roundback.testClient;
+
+import org.tulg.roundback.client.ClientNetwork;
+
+import javax.swing.*;
+import javax.swing.text.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+
+public class FileTransfer extends JDialog {
+    private final TestCliConfig testCliConfig;
+    private final SimpleAttributeSet boldText;
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton buttonCancel;
+    private JTextField txtStorageServer;
+    private JTextField txtPort;
+    private JTextField txtBackupID;
+    private JTextField txtTarget;
+    private JButton btnBrowse;
+    private JEditorPane consoleStorage;
+    private JTextField txtCommand;
+    private JButton btnSend;
+    private JButton btnConnect;
+    private ConnectDiaglog connectDiaglog;
+    private Thread recvThread;
+    private boolean connected;
+    private ClientNetwork clientNetwork;
+    private StyledDocument styledDocument;
+    private SimpleAttributeSet defaultStyle;
+
+
+    public FileTransfer(TestCliConfig testCliConfig) {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        btnBrowse.setMnemonic('b');
+        buttonOK.setMnemonic('o');
+        buttonCancel.setMnemonic('c');
+
+        consoleStorage.setBackground(Color.BLACK);
+        consoleStorage.setForeground(Color.GREEN);
+        consoleStorage.setCaretColor(Color.GREEN);
+        consoleStorage.getCaret().setVisible(true);
+        consoleStorage.getCaret().setBlinkRate(500);
+
+        styledDocument = (StyledDocument) consoleStorage.getDocument();
+        defaultStyle = new SimpleAttributeSet();
+        StyleConstants.setFontFamily(defaultStyle, "Monospace");
+        StyleConstants.setForeground(defaultStyle, Color.GREEN);
+        StyleConstants.setBackground(defaultStyle, Color.BLACK);
+
+        boldText = new SimpleAttributeSet(defaultStyle);
+        StyleConstants.setBold(boldText, true);
+
+        resetThread();
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+        buttonCancel.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        });
+
+
+
+        btnConnect.addActionListener(e -> onConnect());
+        btnSend.addActionListener(e -> onSend());
+
+        this.testCliConfig = testCliConfig;
+        // call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+
+        // call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+    }
+
+    private void onSend() {
+        // Send the entered command
+        if(clientNetwork != null) {
+            try {
+                styledDocument.insertString(styledDocument.getLength(),
+                        ">>> " + txtCommand.getText() + "\n", boldText  );
+                //editorPane1.setText(editorPane1.getText() + "<b>>>> " + txtCommand.getText()+ "</b>\n");
+                clientNetwork.sendRaw(txtCommand.getText());
+                txtCommand.setText("");
+            } catch (IOException | NullPointerException | BadLocationException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void onConnect() {
+        // Connect to the master
+        if(! connected ) {
+            clientNetwork = new ClientNetwork();
+            clientNetwork.setEncryption(testCliConfig.getEncrypted());
+            clientNetwork.setEncryptionKey(testCliConfig.getEncryptionKey());
+            clientNetwork.setServer(txtStorageServer.getText(), txtPort.getText());
+            if (clientNetwork.connect()) {
+                connected = true;
+                btnConnect.setText("Disconnect");
+                contentPane.setPreferredSize(contentPane.getPreferredSize());
+                recvThread.start();
+            }
+        } else {
+            connected = false;
+            btnConnect.setText("Connect");
+            clientNetwork.disconnect();
+            //clientNetwork = null;
+            recvThread.interrupt();
+        }
+    }
+
+    private void onOK() {
+        // TODO: add code to try to send the file
+        // make sure it updates the txt file in the connectDialog box.
+        // should also hide this window, or we need a new console perhaps on
+        // this window.
+    }
+
+    private void onCancel() {
+        // add your code here if necessary
+        dispose();
+    }
+
+    public void setConnectDiaglog(ConnectDiaglog connectDiaglog) {
+        this.connectDiaglog = connectDiaglog;
+    }
+
+
+    private void resetThread(){
+        // set up a new thread to do the recv
+        recvThread = new Thread(() -> {
+            // need to work on this.
+            if(connected) {
+                while(connected) {
+                    // if we ar econnected
+                    try {
+                        String tmpString = clientNetwork.recvRaw();
+                        if(tmpString == null)
+                            throw new IOException("Disconnected.");
+                        if(!tmpString.equals("")) {
+                            styledDocument.insertString(styledDocument.getLength(), tmpString + "\n", defaultStyle);
+                            //editorPane1.setText(editorPane1.getText() + tmpString + "\n");
+                        }
+                    } catch (IOException e) {
+                        connected = false;
+                        btnConnect.setText("Connect");
+                        clientNetwork.disconnect();
+                        clientNetwork = null;
+                    } catch (InterruptedException e) {
+                        connected = false;
+                        btnConnect.setText("Connect");
+                        if(clientNetwork != null) {
+                            clientNetwork.disconnect();
+                            clientNetwork = null;
+                        }
+                    } catch (BadLocationException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            System.out.println("Recieve thread exit.");
+            resetThread();
+        });
+    }
+}
